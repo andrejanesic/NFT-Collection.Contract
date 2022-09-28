7c760f88ab9de5818b08aece1efe8c171e4a8935
Andreja Nesic <andreja.nesic@brerasolutions.com>
2022-09-28 22:44:01 +0200

    minor change

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0xb6CDD6F3a3B182f62e20d94D12D2E8082e2130F2";
+const WHITELIST_CONTRACT_ADDRESS = "0x17E05B9Ce0bA20621f9B052d681B2f6402d3932b";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
